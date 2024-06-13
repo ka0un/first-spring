3834ebdc2332fae2ca223aad038ad32d3036d6c1
kasun <88395585+ka0un@users.noreply.github.com>
2024-06-13 07:36:44 +0530

    init

---
+++ src/test/java/com/hapangama/firstspring/FirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hapangama.firstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
