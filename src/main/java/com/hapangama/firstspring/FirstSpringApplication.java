97102e595aad3dfd5315de73b63c51e4aae78640
kasun <88395585+ka0un@users.noreply.github.com>
2024-06-13 12:11:56 +0530

    created presistence objects

--- src/main/java/com/hapangama/firstspring/FirstSpringApplication.java
+++ src/main/java/com/hapangama/firstspring/FirstSpringApplication.java
@@ -3,10 +3,11 @@ package com.hapangama.firstspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // this is where our microservice starts
 public class FirstSpringApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FirstSpringApplication.class, args);
     }
 
---
+++ src/main/java/com/hapangama/firstspring/domains/Person.java
@@ -0,0 +1,50 @@
+package com.hapangama.firstspring.domains;
+
+import jakarta.persistence.*;
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    private Role title;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Role getTitle() {
+        return title;
+    }
+
+    public void setTitle(Role title) {
+        this.title = title;
+    }
+}
